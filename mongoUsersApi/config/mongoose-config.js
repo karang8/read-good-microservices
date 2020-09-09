b65bafeef011dc14627e7cd42505f222d0fb3157
karang8 <kkaranaro97@gmail.com>
2020-09-10 05:00:22 +0530

    Modified history for removing sensitive data

---
+++ mongoUsersApi/config/mongoose-config.js
@@ -0,0 +1,29 @@
+/* eslint-disable global-require */
+/* eslint-disable no-console */
+
+const mongoose = require('mongoose');
+
+function startConnection() {
+  console.log();
+  const uri = "ENTER URI HERE";
+  mongoose.connect(uri, { useNewUrlParser: true })
+  // const client = new MongoClient(uri, { useNewUrlParser: true });
+  // client.connect(err => {
+  //   const collection = client.db(dbname).collection("devices");
+  //   // perform actions on the collection object
+  //   console.log(err);
+  //   client.close();
+  // });
+
+  // const mongoose = require('mongoose');
+
+  // mongoose.connect(`mongodb://localhost:27017/${dbname}`, { useNewUrlParser: true, useCreateIndex: true });
+
+  // mongoose.connection.once('open', () => {
+  //   console.log('Connection made');
+  // }).on('error', (error) => {
+  //   console.log('Connection error', error);
+  // });
+}
+
+module.exports = { startConnection };
